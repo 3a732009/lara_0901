a72f4b920bdc6c4449106493fa1e6471d216f9f5
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-10 19:45:10 +0800

    練習8:前台路由

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,18 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 class PostsController extends Controller
 {
     public function index()
     {
+        $posts = Post::orderBy('created_at','DESC')->get();
+        $data = [
+            'posts' => $posts,
+        ];
         return view('posts.index');
     }
 
     public function show($id)
     {
-        $data = ['id' => $id];
-
+        $posts = Post::find($id);
+        $data = [
+            'post' => $posts,
+        ];
         return view('posts.show', $data);
     }
 }
